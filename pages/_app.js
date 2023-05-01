77a3933213d6c235c5047f8bd16dae65c27551cf
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-05-01 18:20:43 +0530

    default routing in pages and _app.js entry point experimented

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,6 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <div><Component {...pageProps} />
   <footer>
-  <p>© 2023 Mr.Riddle</p>
+  <p>© 2023 Mr.Tom Riddle</p>
 </footer></div>
 }
